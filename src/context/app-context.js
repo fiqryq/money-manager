c05e746b0951d0172ca0cc7a896d05d4175acfa6
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2022-01-13 22:09:36 +0700

    [add] create global state using context

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -1,2 +1,18 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
 export const AppContext = createContext({});
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
+
+export function AppProvider({ children }) {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("profile"));
+    setUser(data);
+  }, []);
+
+  return <AppContext.Provider value={user}>{children}</AppContext.Provider>;
+}
